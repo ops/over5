197a2215c3dfbfc1793a3617833acfa8772d21a3
Olli Savia <ops@iki.fi>
2019-09-28 19:43:36 +0300

    Fixed warning

--- over5/util.h
+++ over5/util.h
@@ -7,10 +7,11 @@
 
 #include <sys/types.h>
 #include <stdarg.h>
+#include <stdint.h>
 
 u_int8_t *LoadFile(char *);
 void SaveFile(char *, u_int8_t *,u_int32_t);
-void *jalloc(int32_t);
+void *jalloc(int32_t len);
 void jfree(void *);
 u_int32_t jsize(void *); 
 void panic(char *, ...);
